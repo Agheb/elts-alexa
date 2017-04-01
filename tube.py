4dd9d135333b5d905c578bad7c88f927e416376c
Aman Ghebreweldi <amanuelg@me.com>
2017-04-01 11:53:11 +0200

    remove sensitive data

--- tube.py
+++ tube.py
@@ -3,10 +3,11 @@ import json
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from oauth2client.tools import argparser
+from config import youtube_api
 
-DEVELOPER_KEY = "AIzaSyCNGralw9PmUPtnKiryYmKhliR9lkIAzQ4"
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
+DEVELOPER_KEY = youtube_api['key']
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
 
 
 def youtube():
@@ -20,37 +21,37 @@ def channelList():
 
     # response of type dict
 
-    return youtube().playlistItems().list(part="snippet", playlistId="UUsK8wjQhmpPSgRJ55mgxQkw").execute()
+    return youtube().playlistItems().list(part='snippet', playlistId='UUsK8wjQhmpPSgRJ55mgxQkw').execute()
 
 
 def channelStat():
 
-    return youtube().channels().list(part="statistics", id=" UCsK8wjQhmpPSgRJ55mgxQkw").execute()
+    return youtube().channels().list(part='statistics', id=' UCsK8wjQhmpPSgRJ55mgxQkw').execute()
 
 
 def getLatestVid():
 
-    list_response = channelList()["items"][0]["snippet"]
+    list_response = channelList()['items'][0]['snippet']
     results = {}
-    results["title"] = list_response["title"]
-    results["publishedAt"] = list_response["publishedAt"]
-    results["videoId"] = list_response["resourceId"]["videoId"]
+    results['title'] = list_response['title']
+    results['publishedAt'] = list_response['publishedAt']
+    results['videoId'] = list_response['resourceId']['videoId']
 
     return results
 
 
 def currentFollower():
 
-    stat_response = channelStat()["items"][0]["statistics"]["subscriberCount"]
+    stat_response = channelStat()['items'][0]['statistics']['subscriberCount']
     stat_result = {}
-    stat_result["subscriberCount"] = stat_response
+    stat_result['subscriberCount'] = stat_response
 
-    return stat_result["subscriberCount"]
+    return stat_result['subscriberCount']
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     try:
         print getLatestVid()['publishedAt']
     except HttpError, e:
-        print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+        print 'An HTTP error %d occurred:\n%s' % (e.resp.status, e.content)
